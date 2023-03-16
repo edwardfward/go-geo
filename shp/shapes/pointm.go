95e088bb96d3d283a847d3c2bc67e736b09996d9
Edward F. Ward III <4785890+edwardfward@users.noreply.github.com>
2023-03-16 10:50:11 -0500

    addeding parsing logic to pointm struct

--- shp/shapes/pointm.go
+++ shp/shapes/pointm.go
@@ -1,13 +1,31 @@
 package shapes
 
+import (
+	"encoding/binary"
+	"go-shp/utils"
+)
+
 type PointM struct {
 	X float64 // x coordinate
 	Y float64 // y coordinate
 	M float64 // measure
 }
 
-func (p *PointM) Parse(b []byte) {
+func (p *PointM) Parse(r []byte) {
+	_shape := r[0:4] // int32 little endian
+	_x := r[4:12]    // x float64 little endian
+	_y := r[12:20]   // y float little endian
+	_m := r[20:28]   // measure float64 little endian
 
+	var shapeType ShapeType
+	var x, y, m float64
+
+	utils.ReadBinary(_shape, binary.LittleEndian, &shapeType)
+	utils.ReadBinary(_x, binary.LittleEndian, &x)
+	utils.ReadBinary(_y, binary.LittleEndian, &y)
+	utils.ReadBinary(_m, binary.LittleEndian, &m)
+
+	p.X, p.Y, p.M = x, y, m
 }
 
 func (p *PointM) Type() ShapeType {
